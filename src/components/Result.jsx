e645b0723f51bc09d9d627ed969ebc93ee0da780
Alejandro Pablo <ap.alonso3086@gmail.com>
2021-10-04 17:38:19 -0500

    04_10_21-17_37

---
+++ src/components/Result.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Result = ({value})  => {
+    console.log(value);
+    return (
+        <div className="result">
+            {value}
+
+            
+        </div>
+    )
+}
+export default Result
